391f7d96b32e2d2b803fc45827d3f3648f010ead
Cristian <eighttax11@gmail.com>
2021-08-03 17:57:40 -0500

    Se codifico la navegación del sitio

--- portafolio/src/app/app-routing.module.ts
+++ portafolio/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AptitudesHabilidadesComponent } from './components/aptitudes-habilidades/aptitudes-habilidades.component';
+import { DatosAcademicosComponent } from './components/datos-academicos/datos-academicos.component';
+import { DatosPersonalesComponent } from './components/datos-personales/datos-personales.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 const routes: Routes = [
+
+  {
+    path: '', component: DatosPersonalesComponent
+  },
+  {
+    path: 'datos-personales', component: DatosPersonalesComponent
+  },
+  {
+    path: 'datos-academicos', component: DatosAcademicosComponent
+  },
+  {
+    path: 'experiencia-profesional', component: ExperienciaComponent
+  },
+  {
+    path: 'aptitudes-habilidades', component: AptitudesHabilidadesComponent
+  },
+  {
+    path: '**', component: DatosPersonalesComponent
+  }
   
 ];
 
