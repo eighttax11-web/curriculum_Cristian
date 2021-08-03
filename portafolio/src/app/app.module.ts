2b7f5965f5616da02d50f5eb5e85982c91a7c2e9
Cristian <eighttax11@gmail.com>
2021-08-03 17:48:46 -0500

    Creación del componente datos-personales

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DatosPersonalesComponent } from './components/datos-personales/datos-personales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DatosPersonalesComponent
   ],
   imports: [
     BrowserModule,
